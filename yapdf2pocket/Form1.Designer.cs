fcf344fb3bf4ed17f60ad5d9d0d68c056088ba80
acknpop <acknpop@gmail.com>
2015-08-20 01:55:38 +0900

    Initial

---
+++ yapdf2pocket/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace yapdf2pocket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 47);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Paper size";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 18);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(186, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(153, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save as PocketMod";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(217, 22);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(53, 16);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "frame";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 98);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "YA PDF to PocketMod";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ yapdf2pocket/Form1.cs
@@ -0,0 +1,340 @@
+﻿/*
+Copyright (c) 2015, acknack
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, 
+  this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, 
+  this list of conditions and the following disclaimer in the documentation 
+  and/or other materials provided with the distribution.
+* Neither the name of the <organization> nor the　names of its contributors 
+  may be used to endorse or promote products derived from this software 
+  without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System;
+using System.Data;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using System.Reflection;
+
+namespace yapdf2pocket
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        SaveFileDialog sfd = new SaveFileDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Setup comboBox.
+            comboBox1.Items.Clear();
+            Assembly a = Assembly.LoadFrom(".\\itextsharp.dll");
+            foreach (Type t in a.GetExportedTypes())
+            {
+                if (t.Name == "PageSize")
+                {
+                    FieldInfo[] f = t.GetFields();
+                    foreach (FieldInfo fi in f)
+                    {
+                        if (fi.FieldType.Name == "Rectangle")
+                            comboBox1.Items.Add(fi.Name);
+                    }
+                }
+            }
+            var index = comboBox1.FindStringExact("A4");
+            comboBox1.SelectedIndex = index;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "PDF files(*.pdf)|*.pdf";
+            ofd.Title = "Select a PDF file.";
+            //ofd.RestoreDirectory = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //CheckPdfProtection(ofd.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ofd.FileName == "")
+            {
+                MessageBox.Show("PDF file is not selected.",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+
+            sfd.Filter = "PDF files(*.pdf)|*.pdf";
+            sfd.Title = "Save PocketMod PDF";
+            //sfd.RestoreDirectory = true;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                if (ofd.FileName == sfd.FileName)
+                {
+                    MessageBox.Show("Same PDF file could not be accepted.",
+                        "Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    return;
+                }
+                pocketmodstyle();
+            }
+        }
+
+        private void pocketmodstyle()
+        {
+
+            PdfReader pr = new PdfReader(ofd.FileName);
+            if (pr.IsEncrypted())
+            {
+                MessageBox.Show(ofd.FileName + " is protected.",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+
+
+            iTextSharp.text.Rectangle dst = PageSize.GetRectangle(comboBox1.SelectedItem.ToString());
+
+            Document doc = new Document(dst);
+
+            PdfWriter pw = PdfWriter.GetInstance(doc, new FileStream(sfd.FileName, FileMode.Create));
+
+            doc.Open();
+
+            for (int i = 1; i <= pr.NumberOfPages; i++)
+            {
+                if (i % 8 == 1) doc.NewPage();
+
+                PdfContentByte pcb = pw.DirectContent;
+
+                // No care to rotation.
+                //iTextSharp.text.Rectangle src = pr.GetPageSize(i);
+                iTextSharp.text.Rectangle src = pr.GetPageSizeWithRotation(i);
+                int rot = pr.GetPageRotation(i);
+                Console.WriteLine(rot);
+                Console.WriteLine("{0:F}, {1:F}\n",src.Width, src.Height);
+
+                float scale = dst.Height / (src.Width * 4);
+                float offset = (dst.Width / 2 - (scale * src.Height)) / 2;
+
+                PdfImportedPage page;
+                page = pw.GetImportedPage(pr, i);
+
+                // Affine translation of PocketMod style.
+                switch (i % 8)
+                {
+                    case 1:
+                        if (rot == 90 || rot == 270)
+                        {
+                            pcb.AddTemplate(page, 
+                                scale, 0f, 0f, scale,
+                                offset, dst.Height * 3 / 4);
+                        } 
+                        else
+                        {
+                            // Counterclockwise 90 degrees
+                            pcb.AddTemplate(page, 
+                                0f, scale, -scale, 0f, 
+                                (dst.Width / 2) - offset, dst.Height * 3 / 4);
+                        }
+                        break;
+
+                    case 2:
+                        if (rot == 90 || rot == 270)
+                        {
+                            pcb.AddTemplate(page,
+                                scale, 0f, 0f, scale,
+                                (dst.Width / 2) + offset, dst.Height * 3 / 4);
+                        }
+                        else
+                        {
+                            // Clockwise 90 degrees
+                            pcb.AddTemplate(page, 
+                                0f, -scale, scale, 0f, 
+                                (dst.Width / 2) + offset, dst.Height);
+                        }
+                        break;
+                    case 3:
+                        if (rot == 90 || rot == 270)
+                        {
+                            pcb.AddTemplate(page,
+                                scale, 0f, 0f, scale,
+                                (dst.Width / 2) + offset, dst.Height * 2 / 4);
+                        }
+                        else
+                        {
+                            // Clockwise 90 degrees
+                            pcb.AddTemplate(page, 
+                                0f, -scale, scale, 0f, 
+                                (dst.Width / 2) + offset, dst.Height * 3 / 4);
+                        }
+                        break;
+                    case 4:
+                        if (rot == 90 || rot == 270)
+                        {
+                            pcb.AddTemplate(page,
+                                scale, 0f, 0f, scale,
+                                (dst.Width / 2) + offset, dst.Height / 4);
+                        }
+                        else
+                        {
+                            // Clockwise 90 degrees
+                            pcb.AddTemplate(page, 
+                                0f, -scale, scale, 0f, 
+                                (dst.Width / 2) + offset, dst.Height * 2 / 4);
+                        }
+                        break;
+                    case 5:
+                        if (rot == 90 || rot == 270)
+                        {
+                            pcb.AddTemplate(page,
+                                scale, 0f, 0f, scale,
+                                (dst.Width / 2) + offset, 0);
+                        }
+                        else
+                        {
+                            // Clockwise 90 degrees
+                            pcb.AddTemplate(page, 
+                                0f, -scale, scale, 0f, 
+                                (dst.Width / 2) + offset, dst.Height / 4);
+                        }
+                        break;
+
+                    case 6:
+                        if (rot == 90 || rot == 270)
+                        {
+                            pcb.AddTemplate(page,
+                                scale, 0f, 0f, scale,
+                                offset, 0);
+                        }
+                        else
+                        {
+                            // Counterclockwise 90 degrees
+                            pcb.AddTemplate(page, 
+                                0f, scale, -scale, 0f, 
+                                (dst.Width / 2) - offset, 0);
+                        }
+                        break;
+                    case 7:
+                        if (rot == 90 || rot == 270)
+                        {
+                            pcb.AddTemplate(page,
+                                scale, 0f, 0f, scale,
+                                offset, dst.Height / 4);
+                        }
+                        else
+                        {
+                            // Counterclockwise 90 degrees
+                            pcb.AddTemplate(page, 
+                                0f, scale, -scale, 0f, 
+                                (dst.Width / 2) - offset, dst.Height / 4);
+                        }
+                        break;
+                    case 0:
+                        if (rot == 90 || rot == 270)
+                        {
+                            pcb.AddTemplate(page,
+                                scale, 0f, 0f, scale,
+                                offset, dst.Height * 2 / 4);
+                        }
+                        else
+                        {
+                            // Counterclockwise 90 degrees
+                            pcb.AddTemplate(page,
+                                0f, scale, -scale, 0f, 
+                                (dst.Width / 2) - offset, dst.Height * 2 / 4);
+                        }
+                        break;
+                }
+
+                if ((i % 8 == 0)||(i == pr.NumberOfPages))
+                {
+                    // Draw guide line for folding.
+                    pcb.SetLineWidth(0.01f);
+
+                    if (checkBox1.Checked)
+                    {
+                        pcb.MoveTo(0f, 0f);
+                        pcb.LineTo(dst.Width, 0f);
+                        pcb.LineTo(dst.Width, dst.Height);
+                        pcb.LineTo(0f, dst.Height);
+                        pcb.LineTo(0f, 0f);
+                        pcb.Stroke();
+                    }
+
+                    pcb.MoveTo(0f, dst.Height * 3 / 4f);
+                    pcb.LineTo(dst.Width, dst.Height * 3 / 4);
+                    pcb.Stroke();
+
+                    pcb.MoveTo(0f, dst.Height * 2 / 4f);
+                    pcb.LineTo(dst.Width, dst.Height * 2 / 4);
+                    pcb.Stroke();
+
+                    pcb.MoveTo(0f, dst.Height * 1 / 4f);
+                    pcb.LineTo(dst.Width, dst.Height * 1 / 4);
+                    pcb.Stroke();
+
+                    pcb.MoveTo(dst.Width / 2, 0f);
+                    pcb.LineTo(dst.Width / 2, dst.Height * 1 / 4);
+                    pcb.Stroke();
+
+                    pcb.MoveTo(dst.Width / 2, dst.Height * 3 / 4);
+                    pcb.LineTo(dst.Width / 2, dst.Height);
+                    pcb.Stroke();
+
+                    pcb.SetLineDash(3f, 3f);
+                    pcb.MoveTo(dst.Width / 2, dst.Height * 1 / 4);
+                    pcb.LineTo(dst.Width / 2, dst.Height * 3 / 4);
+                    pcb.Stroke();
+
+                    pcb.SetLineDash(0);
+                }
+
+            }
+
+            doc.Close();
+            pw.Close();
+            pr.Close();
+        }
+/*
+        private void CheckPdfProtection(string filePath)
+        {
+            try
+            {
+                PdfReader reader = new PdfReader(filePath);
+                if (!reader.IsEncrypted()) return;
+                if (!PdfEncryptor.IsPrintingAllowed((int)reader.Permissions))
+                    throw new InvalidOperationException("the selected file is print protected and cannot be imported");
+                if (!PdfEncryptor.IsModifyContentsAllowed((int)reader.Permissions))
+                    throw new InvalidOperationException("the selected file is write protected and cannot be imported");
+            }
+            //catch (BadPasswordException) { throw new InvalidOperationException("the selected file is password protected and cannot be imported"); }
+            catch (BadPdfFormatException) { throw new InvalidDataException("the selected file is having invalid format and cannot be imported"); }
+        }
+*/
+    }
+}
+
